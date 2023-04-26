ccb8096ce7453dca1d3de444dbfea294fd5d1a3f
Thomas Batteux <tbatteux@c1r2p3.42lehavre.fr>
2023-04-26 17:22:08 +0200

    test maison

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tbatteux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 09:58:29 by tbatteux          #+#    #+#             */
-/*   Updated: 2023/04/26 12:38:01 by tbatteux         ###   ########.fr       */
+/*   Updated: 2023/04/26 16:58:25 by tbatteux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ char	*recup_reste(char *lu)
 	{
 		while (lu[i] != '\n')
 			i++;
+		i++;
 		while (lu[i])
 			reste[j++] = lu[i++];
 	}
@@ -80,26 +81,35 @@ char	*recup_ligne(char *lu, int ret, int fd)
 	}
 	ligne[i] = 0;
 	free(lu);
-	printf("%s", ligne);
 	return (ligne);
 }
 
 char	*get_next_line(int fd)
 {
 	char	*lu;
-	int	ret;
-	static char	reste[] = "";
+	int		ret;
+	static char	*reste = NULL;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
+	if (reste == NULL)
+	{
+		reste = malloc(sizeof(char *));
+		if (!reste)
+			return (NULL);
+		reste[0] = 0;
+	}
 	lu = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!lu)
 		return (NULL);
 	ret = read(fd, lu, BUFFER_SIZE);
 	lu = ft_strjoin(reste, lu);
-	//reste = recup_reste(lu);
 	printf("%s\n", reste);
+	reste = recup_reste(lu);
+	printf("reste :%s\n", reste);
 	lu = recup_ligne(lu, ret, fd);
+	printf("fonction ligne \n");
+	printf("reste :%s\n", reste);
 	return (lu);
 }
 
@@ -111,7 +121,7 @@ int	main()
 	if (fd == -1)
 		return (1);
 	get_next_line(fd);
-	get_next_line(fd);
+	printf("\n\n\n");
 	get_next_line(fd);
 	if (close(fd) != -1);
 		return (0);
